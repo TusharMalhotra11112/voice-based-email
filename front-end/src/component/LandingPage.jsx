1846371d2428845c11bc8ec606c624f2dcf7ac41
Tushar Malhotra <malhotratushar999@gmail.com>
2023-10-06 16:01:46 +0530

    working ui

--- front-end/src/component/LandingPage.jsx
+++ front-end/src/component/LandingPage.jsx
@@ -1,15 +1,19 @@
-import React from 'react'
+import React, { useState } from 'react'
 import Login from './Login'
 import SignUp from './SignUp'
 import VoiceRec from './voiceRec'
 import Testing from './Testing'
 import Newtest from './Newtest'
-export default function login() {
+export default function LandingPage() {
+  let [signUpNo,setSignUpNo] = useState(0)
+  const handelSignUpNo =(num)=>{
+    setSignUpNo(num)
+  }
   return (
     <div className='loginPage'>
         {/* <img src="loginbg.jpg" alt="loginbg" className='loginBg'/> */}
         {/* <Login/> */}
-        <SignUp/>
+        <SignUp signUpNo={signUpNo} handelSignUpNo={handelSignUpNo} />
         {/* <VoiceRec/> */}
         {/* <Testing/> */}
         {/* <Newtest /> */}
--- front-end/src/component/SignUp.jsx
+++ front-end/src/component/SignUp.jsx
@@ -2,8 +2,9 @@ import { Button, IconButton, TextField } from '@mui/material'
 import React, { useEffect, useState } from 'react'
 import VoiceRec from './voiceRec'
 import SpeechRecognition,{useSpeechRecognition} from 'react-speech-recognition';
+import Speech from 'react-text-to-speech'
 
-export default function SignUp() {
+export default function SignUp({signUpNo,handelSignUpNo}) {
   const {
     transcript,
     listening,
@@ -15,178 +16,103 @@ export default function SignUp() {
   const [email,setEmail] = useState("")
   const [pass,setPass] = useState("")
   const [text,setText] = useState(transcript)
-  var num = -1
-  var change = 0
-
-
-  const synth = window.speechSynthesis;
-
-
+  // const synth = window.speechSynthesis;
+  // const u = new SpeechSynthesisUtterance(text)
+  // synth.speak(u)
+  
   const say = (text)=>{
-    resetTranscript()
-    const u = new SpeechSynthesisUtterance(text)
-    synth.speak(u)
-    console.log(text)
-  }
-
-  const startListen = ()=>{
-    resetTranscript()
-    SpeechRecognition.startListening({continuous:true})
-  }
-
-  const stopListen = ()=>{
-    SpeechRecognition.stopListening()
-    return transcript
-  }
-
-
-  // console.log("Listening")
-  // if (browserSupportsSpeechRecognition) { 
-    //   SpeechRecognition.startListening()
-    //   setTimeout(() => {
-    //     SpeechRecognition.stopListening()
-    //   }, 3000);
-    //   console.log(transcript)
-    // }
-    // else{
-    //   console.log("not supported")
-    // }
-    // console.log(transcript)
-  // const listen = async()=>{
-  //   return new Promise(async(res,rej)=>{
-  //     console.log("Listening")
-  //     if (browserSupportsSpeechRecognition) { 
-  //       await SpeechRecognition.startListening({continuous:true})
-  //       setTimeout(async() => {
-  //         await SpeechRecognition.stopListening()
-  //         console.log(transcript)
-  //         // console.log(SpeechRecognition)
-  //         res(transcript)
-  //       }, 5000);
-  //     }
-  //     else if(transcript === null){
-  //       rej("rejected")
-  //     }
-  //     else{
-  //       console.log("not supported")
-  //       rej("rejected")
-  //     }
-
-  //   })
-  // }
-
-  const handelYesNo = ()=>{
-    if(transcript === 'yes' || 'Yes' || 'Yes.'){
-      console.log("yes...")
-      return true
-    }
-    else if(transcript === 'no' || 'No' || "No."){
-      console.log("no...")
-      return false
-    }
-    else{
-      if(num === 0){
-        checkEmail()
-      }
-      else if(num === 1){
-        checkPass()
-      }
-    }
-  }
-
-  const checkEmail = ()=>{
-    num = -1
-    say(`Is your email id ${email} ? say yes or no`)
-    startListen()
-    setTimeout(()=>{
-      stopListen()
+    return new Promise((res,rej)=>{
+      console.log(text)
+      const synth = window.speechSynthesis;
+      const u = new SpeechSynthesisUtterance(text)
+      synth.speak(u)
       setTimeout(()=>{
-        if(handelYesNo()){
-          change = 1
-          num = 1
-          managePass()
-        }
-        else{
-          manageEmail()
-        }
-      },2000)
-    },5000)
-    
+        // synth.cancel()
+        res('')
+      },3000)
+
+    })
   }
+
+
+  const listen = ()=>{
+    console.log("listening")
+    return new Promise((res,rej)=>{
+      resetTranscript()
+      say("now")
+      .then(SpeechRecognition.startListening({continuous:true}))
+      setTimeout(()=>{
+        SpeechRecognition.stopListening()
+        res(transcript)
+      },5000)
+    })
+  }
+  
 
   const manageEmail = ()=>{
-    say("please Enter your Email id")
-    num = 0
-    startListen()
-    setTimeout(()=>{
-      stopListen()
-      setTimeout(()=>{
-        checkEmail()
-      },2000)
-    },5000)
-
-  }
-  const checkPass = ()=>{
-    say(`Is your password ${pass} ? say yes or no`)
-    startListen()
-    setTimeout(()=>{
-      stopListen()
-      setTimeout(()=>{
-        if(handelYesNo()){
-          change = 2
-        }
-        else{
-          managePass()
-        }
-      },2000)
-    },5000)
+    return new Promise((res,rej)=>{
+      say("please Say your Email")
+      .then(()=>{
+        listen()
+        .then(()=>{
+          res('')
+        })
+      })
+    })
   }
 
-  const managePass=()=>{
-  say("please Enter your password")
-      startListen()
-      setTimeout(()=>{
-        stopListen()
-        setTimeout(()=>{
-          checkPass()
-        },2000)
-      },5000)
-    }
-    
-    useEffect(()=>{
-      setText(transcript)
-  
-      if(num === 0){
-        setEmail(transcript)
-      }
-      else if(num === 1){
-        setPass(transcript)
-      }
-    },[transcript])
-    
+  const managePass = ()=>{
+    return new Promise((res,rej)=>{
+      say("please Say your Password")
+      .then(()=>{
+        listen()
+        .then(()=>{
+          res('')
+        })
+      })
+    })
+  }
+
+
+
   useEffect(()=>{
-    say("Hello Welcome to the sign-Up page")
-    setTimeout(()=>{
-      if(change === 0){
+      say("Hello welcome to the Sign-Up page")
+      .then(()=>{
         manageEmail()
-      }
-      else if(change === 1){
-        managePass()
-      }
-    },1000)
-  },[change])
+        .then(()=>{
+          handelSignUpNo(1)
+          managePass()
+        })
+      })
+  },[])
 
-
+  useEffect(()=>{
+    console.log(transcript)
+    if(signUpNo === 0){
+      setEmail(transcript)
+    }
+    else if(signUpNo === 1){
+      setPass(transcript.toLowerCase())
+    }
+  },[transcript])
+  
+  
+if (!browserSupportsSpeechRecognition) {
+  return (
+    <div>Speech Recognition is not supported in this browser</div>
+  )
+} 
 
   return (
     <div className='loginTab'>
         <p className="loginText">Sign-Up</p>
         <TextField id="standard-basic" label="Email-Id" variant="standard" className='loginEmail' value={email}/>
-        <TextField id="standard-basic" label="Password" variant="standard" type='password' className='loginPassword' value={pass}/>
+        <TextField id="standard-basic" label="Password" variant="standard" className='loginPassword' value={pass}/>
         <VoiceRec/>
         <VoiceRec/>
         <VoiceRec/>
-        <Button variant="contained" className='loginBtn' onClick={()=>console.log(`Email: ${email} Password: ${pass} transcript:${transcript}`)}>Sign Up</Button>
+        <Button variant="contained" className='loginBtn'
+        onClick={()=>console.log(`Email: ${email} Password: ${pass} transcript:${transcript}`)}>Sign Up</Button>
         <p className="loginSignupText">Already have an Account?<span className='toSignUp'>Log In</span></p>
     </div>
   )
