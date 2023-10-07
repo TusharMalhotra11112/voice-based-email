7858d13e8b849ef7185eb2ada57652b166c2a916
shobhit <shobhitmishra2002@gmail.com>
2023-10-07 19:45:08 +0530

    added code for uploading wav files in fastapi

--- back-end/main.py
+++ back-end/main.py
@@ -1,7 +1,38 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, File, UploadFile
+from typing_extensions import Annotated
+import time
+from typing import List
 
 app = FastAPI()
 
 @app.get("/")
 async def root():
     return {"message":"Hello world"}
+
+@app.post("/upload/")
+async def upload(files: Annotated[List[UploadFile], File(description="Multiple wav files to upload")]):
+    
+    # data = file.file.read()
+    # extention = file.filename[-4:]
+    
+    # targetFile = open("uploads/out_" + str(time.time()).split('.')[0] + extention, "wb")
+    # targetFile.write(data)
+
+    # res: List[str] = []
+    # for file in files:
+    #     res.append(file.filename)
+
+    storedFileNames: List[str] = []
+
+    for file in files:
+        data = file.file.read()
+        ext = file.filename[-4:]
+
+        targetFileName = "uploads/" + file.filename[0:-4] + str(time.time()).split('.')[0] + ext
+
+        targetFile = open(targetFileName, "wb")
+        targetFile.write(data)
+
+        storedFileNames.append(file.filename)
+
+    return {"uploadedFiles": storedFileNames}
