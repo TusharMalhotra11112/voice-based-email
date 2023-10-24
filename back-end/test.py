17c828e5104f0926c022c4365b5a68dc904a89b2
shobhit <shobhitmishra2002@gmail.com>
2023-10-24 22:14:48 +0530

    added some changes in test.py

--- back-end/test.py
+++ back-end/test.py
@@ -10,10 +10,12 @@ engine = create_engine(password)
 # function to create sample.wav file from the database. 
 def createWavFile():
     with engine.connect() as conn:
-        result = conn.execute(text("SELECT audio_1 FROM user_data WHERE id = 1"))
-        
-        if(result.all()):
-            audioBinary = result.all()[0][0]
+        result = conn.execute(text("SELECT audio_1 FROM user_data WHERE id = 1;"))
+
+        data = result.all()
+
+        if(len(data)):
+            audioBinary = data[0][0]
             file = open("sample.wav", "wb")
             file.write(audioBinary)
         else:
