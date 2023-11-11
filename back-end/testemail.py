2eac28bf39d8a66023fc787d7c4e037bb68e42e2
shobhit <shobhitmishra2002@gmail.com>
2023-11-11 10:22:31 +0530

    testing of email system is completed

--- back-end/testemail.py
+++ back-end/testemail.py
@@ -4,6 +4,8 @@ app password: bjdz ejfw lqwb cree
 '''
 
 import smtplib, ssl
+from email.mime.text import MIMEText
+from email.mime.multipart import MIMEMultipart
 
 port = 465
 smtp_server = "smtp.gmail.com"
@@ -12,6 +14,11 @@ password = "bjdz ejfw lqwb cree"
 sender_email = "shobhitmishra2002@gmail.com"
 receiver_email = "shobhitmishra2002@gmail.com"
 
+message = MIMEMultipart('alternative')
+message["Subject"] = "multiplepart test"
+message["From"] = sender_email
+message["To"] = receiver_email
+
 text = """
 Subject: Hi there
 
@@ -19,14 +26,17 @@ Subject: Hi there
 This message if from python.
 """
 
+part = MIMEText(text, "plain")
+message.attach(part)
+
 context = ssl.create_default_context()
 
 try:
     server = smtplib.SMTP_SSL(smtp_server, port, context=context)
     server.ehlo()
     server.login(sender_email, password)
+    server.sendmail(sender_email, receiver_email, message.as_string())
     print("Done")
-    # server.sendmail(sender_email, receiver_email, text)
 except Exception as e:
     print(e)
 finally:
